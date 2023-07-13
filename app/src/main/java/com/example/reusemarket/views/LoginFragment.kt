2c9f7fea5e162dc5c0bdf06b7f7098da5f7ec080
Theekshana <tvimukthi.m@gmail.com>
2023-07-13 20:51:39 +0900

    add login function

--- app/src/main/java/com/example/reusemarket/views/LoginFragment.kt
+++ app/src/main/java/com/example/reusemarket/views/LoginFragment.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.reusemarket.R
 import com.example.reusemarket.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
 
-    lateinit var binding: FragmentLoginBinding
+    private lateinit var binding: FragmentLoginBinding
+    private lateinit var viewModel: AuthViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +23,11 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        viewModel = ViewModelProvider(requireActivity())[AuthViewModel::class.java]
+
+        binding.vm = viewModel
+        binding.lifecycleOwner = this
 
         return binding.root
 
@@ -27,5 +37,30 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.btnLogin.setOnClickListener {
+
+            // Retrieve the email and password values from the EditText
+            val strEmail = binding.etEmail.text.toString()
+            val strPassword = binding.etPassword.text.toString()
+
+            // Initiates the sign-up process
+            viewModel.signUpInfo(strEmail, strPassword)
+            //viewModel.login(strEmail,strPassword)
+            //navigate to the list fragment
+            navigateToListFragment()
+            Toast.makeText(context, "account added", Toast.LENGTH_LONG).show()
+
+        }
+
+
     }
+
+    /**
+     * Navigates to the List Fragment.
+     */
+    private fun navigateToListFragment() {
+        findNavController().navigate(R.id.action_loginFragment_to_listFragment)
+    }
+
+
 }
