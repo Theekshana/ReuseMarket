5bd41c0d55327d83ef3ddcb9f63e1d99ad8c058d
Theekshana <tvimukthi.m@gmail.com>
2023-07-13 20:15:59 +0900

    add data binding

--- app/src/main/java/com/example/reusemarket/views/ListFragment.kt
+++ app/src/main/java/com/example/reusemarket/views/ListFragment.kt
@@ -6,18 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.reusemarket.R
-
+import com.example.reusemarket.databinding.FragmentListBinding
 
 
 class ListFragment : Fragment() {
 
+    lateinit var binding: FragmentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        binding = FragmentListBinding.inflate(inflater, container, false)
+
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
 
