6541e6b973130f3fcc23c2bd5019fdceb3383af6
Theekshana <tvimukthi.m@gmail.com>
2023-07-22 14:33:37 +0900

    create suspend function for add data

--- app/src/main/java/com/example/reusemarket/repository/DataRepository.kt
+++ app/src/main/java/com/example/reusemarket/repository/DataRepository.kt
@@ -1,6 +1,12 @@
 package com.example.reusemarket.repository
 
+import com.example.reusemarket.constants.UIState
+import com.example.reusemarket.model.Data
+import com.google.android.gms.tasks.Task
+
 interface DataRepository {
 
-    suspend fun addData(data: )
+    suspend fun addData(data: Data): Result<UIState>
+
+
 }
--- app/src/main/java/com/example/reusemarket/repository/DataRepositoryImpl.kt
+++ app/src/main/java/com/example/reusemarket/repository/DataRepositoryImpl.kt
@@ -1,4 +1,31 @@
 package com.example.reusemarket.repository
 
-class DataRepositoryImpl {
+import com.example.reusemarket.constants.UIState
+import com.example.reusemarket.model.Data
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import java.lang.Exception
+import javax.inject.Inject
+
+class DataRepositoryImpl @Inject constructor(
+    private val firestore: FirebaseFirestore
+    ): DataRepository{
+
+    private val itemData = firestore.collection("items")
+    override suspend fun addData(data: Data): Result<UIState> {
+        return withContext(Dispatchers.IO){
+            try {
+                itemData.add(data).await()
+                Result.success(UIState.Success("Data Added"))
+
+            }catch (e: Exception){
+                Result.failure(e)
+
+            }
+        }
+    }
+    
+
 }
