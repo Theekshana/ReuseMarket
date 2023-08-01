517258291f1cebc92f666d249ac16ce841e25409
Theekshana <tvimukthi.m@gmail.com>
2023-08-01 23:49:49 +0900

    add the update_item fragment UI

--- app/src/main/java/com/example/reusemarket/adapters/UserItemAdapter.kt
+++ app/src/main/java/com/example/reusemarket/adapters/UserItemAdapter.kt
@@ -1,25 +1,29 @@
 package com.example.reusemarket.adapters
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.reusemarket.R
+import com.example.reusemarket.databinding.UserItemListBinding
 import com.example.reusemarket.model.UserItem
+import com.example.reusemarket.views.data.AddItemFragment
 
 class UserItemAdapter(
-    private var userItem:  List<UserItem>
+    private var userItem: List<UserItem>,
 ) : RecyclerView.Adapter<UserItemAdapter.UserItemViewHolder>() {
 
-    inner class UserItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val textView = itemView.findViewById<TextView>(R.id.itemName)
+    inner class UserItemViewHolder(val binding: UserItemListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserItemViewHolder {
 
-        val view =LayoutInflater.from(parent.context).inflate(R.layout.user_item_list, parent, false)
-        return UserItemViewHolder(view)
+        val itemView =
+            UserItemListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return UserItemViewHolder(itemView)
 
     }
 
@@ -29,18 +33,12 @@ class UserItemAdapter(
 
     override fun onBindViewHolder(holder: UserItemViewHolder, position: Int) {
 
-       // holder.textView.text = userItem[position]
+        // holder.textView.text = userItem[position]
         val currentItem: UserItem = userItem[position]
 
         val tvNumber: TextView = holder.itemView.findViewById(R.id.itemName)
 
         tvNumber.text = currentItem.text1
-
-
-
-
-
-
 
 
     }
