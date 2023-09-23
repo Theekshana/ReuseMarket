5cdf5145e87e70c6633a7c684612220b8e2d2daa
Theekshana <tvimukthi.m@gmail.com>
2023-09-23 13:30:26 +0530

    change data class

--- app/src/main/java/com/example/reusemarket/model/AllItem.kt
+++ app/src/main/java/com/example/reusemarket/model/AllItem.kt
@@ -11,6 +11,9 @@ data class AllItem(
     val itemImage: Uri? = null,
     val name: String? = null,
     val category: String? = null,
-    var email: String? = null
+    var email: String? = null,
+    var location: String? = null,
+    var phoneNumber: String? = null,
+    var description: String? = null,
 
 ): Parcelable
